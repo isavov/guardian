bdc6c6208cbe11273b728f9e42b170986ae52579
Ihar <igor.tsykala@envisionblockchain.com>
2024-06-03 14:38:52 +0500

    Revert "feat: base vertion invalidation cache"
    This reverts commit 7fad10bc46c056530d30493def0dd1412bed7813.

--- api-gateway/src/helpers/interceptors/utils/hash.ts
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto';
-
-//types and interfaces
-import { IUser } from '@guardian/interfaces';
-
-export function getHash(user: IUser): string {
-  return crypto.createHash('md5').update(JSON.stringify(user)).digest('hex');
-}
--- api-gateway/src/helpers/interceptors/utils/index.ts
+++ api-gateway/src/helpers/interceptors/utils/index.ts
@@ -1,5 +1,1 @@
 export { getFileFromPart } from './multipart.js';
-
-export { getHash } from './hash.js';
-
-export { getCacheKey } from './cache.js';
